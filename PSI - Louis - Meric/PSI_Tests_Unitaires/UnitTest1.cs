c9ee258cc59a94bd6245a9f81092eb2f6866134d
Méric LALLEMENT <meric.lallement@edu.devinci.fr>
2022-05-01 21:56:03 +0200

    .

--- PSI - Louis - Meric/PSI_Tests_Unitaires/UnitTest1.cs
+++ PSI - Louis - Meric/PSI_Tests_Unitaires/UnitTest1.cs
@@ -37,9 +37,9 @@ namespace PSI___Louis___Meric
 
         public void TestConvert2CharTo11Bits()
         {
-            char[] he = { 'h', 'e' };
+            char[] he = { 'H', 'E' };
             int[] attendu = { 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1 };
-            int[] result = MyImage.Convert2CharTo11Bits(he);        //result est null pcq convert2charTo11Bits appelle une autre méthode, jsp comment faire
+            int[] result = MyImage.Convert2CharTo11Bits(he);        
             bool validé = true;
             for(int i = 0; i < attendu.Length; i++)
             {
@@ -48,5 +48,46 @@ namespace PSI___Louis___Meric
 
             Assert.AreEqual(validé, true);
         }
+
+        [TestMethod]
+        public void TestConvertStringToTabBinaire()
+        {
+            string entrée = "HELLO WORLD";
+            int[][] result = MyImage.ConvertStringToTabBinaire(entrée);
+
+            int[][] attendu = new int[][]
+            {
+                new int[] { 0,1,1,0,0,0,0,1,0,1,1 },
+                new int[] { 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0 },
+                new int[] {1,0,0,0,1,0,1,1,1,0,0 },
+                new int[] {1,0,1,1,0,1,1,1,0,0,0 },
+                new int[] { 1,0,0,1,1,0,1,0,1,0,0 },
+                new int[] {0,0,1,1,0,1}
+            };
+            bool validé = true;
+            for(int i = 0; i < 5;i++)
+            {
+                for (int j = 0; j < 11; j++)
+                    if (attendu[i][j] != result[i][j]) validé = false;
+            }
+            for (int j = 0; j < 6; j++)
+                if (result[5][j] != attendu[5][j]) validé = false;
+
+            Assert.AreEqual(validé, true);
+        }
+
+        [TestMethod]
+        public void TestAlphanumToChar()
+        {
+            int entrée = 36;
+            string attendu = " ";
+            string result = MyImage.ConvertAlphanumToChar(entrée);
+            bool validé = false;
+            if (attendu == result) validé = true;
+
+            Assert.AreEqual(validé, true);
+
+        }
+      
     }
 }
